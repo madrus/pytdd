42e6b456acbacb493a715b681f819739c62a8108
madrus <madrus@gmail.com>
2016-08-03 16:29:18 +0200

    worked up to page 60\nbut am getting a strange error

--- func_tests.py
+++ func_tests.py
@@ -1,5 +1,6 @@
 import unittest
 import time
+from lists.models import Item
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -73,6 +74,26 @@ class NewVisitorTest(unittest.TestCase):
 
         # Satisfied, she goes back to sleep
         self.fail('Finish the test!')
+
+
+class ItemModelTest(unittest.TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
 
 
 if __name__ == '__main__':
